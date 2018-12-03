0b518c1c306070fa7c4723793827c2f4648f7602
KaizokuBento <31934447+KaizokuBento@users.noreply.github.com>
2018-12-03 09:10:51 +0100

    Version 0.1 hopefully without bugs.

--- Poke-Farm-QoL.user.js
+++ Poke-Farm-QoL.user.js
@@ -2,79 +2,79 @@
 // @name         Poké Farm QoL
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Quality of Life chan
+// @description  Quality of Life changes for Poké Farm!
 // @author       Bentomon
-// @downloadURL  https://raw.githubusercontent.com/KaizokuBento/PokeFarmShelter/master/Poke-Farm-QoL.js
 // @match        https://pokefarm.com/shelter
+// @downloadurl  https://github.com/KaizokuBento/PokeFarmShelter/edit/master/Poke-Farm-QoL.user.js
 // @require      http://code.jquery.com/jquery-3.3.1.js
 // @grant        GM_addStyle
 // ==/UserScript==
 
 (function($) {
     'use strict';
-
         /////////////////////////////////////
-        // Here starts the script code!    //
+        // Welcome to my first ever script!//
         // Let's setup the Settings first. //
         /////////////////////////////////////
 
-    //const DEFAULT_USER_SETTINGS = {
-    //        noIdea: false,
-    //        doYouHaveAnIdea: true,
-    //    };
+    const DEFAULT_USER_SETTINGS = {
+    noUserSettingsYet: true,
+    aLotOfJavascriptExperience: false,
+    };
 
-    //    const SETTINGS_KEY = 'UserSettings';
+    const SETTINGS_KEY = 'UserSettings';
 
-        /////////////////////////////////////
-        // Changes to CSS                  //
-        /////////////////////////////////////
+        ///////////////////////////////////////
+        // Changes to CSS                    //
+        // I should probably put this in Git.//
+        ///////////////////////////////////////
 
     const PFQOL_STYLES = `
     /* Tooltip container */
 .tooltip {
-    position: relative;
-    display: inline-block;
-    border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
+position: relative;
+display: inline-block;
+border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
 }
 
-/* Tooltip text */
+    /* Tooltip text */
 .tooltip .tooltiptext {
-    visibility: hidden;
-    width: 500px;
-    background-color: #555;
-    color: #fff;
-    text-align: center;
-    padding: 5px 0;
-    border-radius: 6px;
+visibility: hidden;
+width: 500px;
+background-color: #555;
+color: #fff;
+text-align: center;
+padding: 5px 0;
+border-radius: 6px;
 
     /* Position the tooltip text */
-    position: absolute;
-    z-index: 1;
-    bottom: 125%;
-    left: 50%;
-    margin-left: -60px;
+position: absolute;
+z-index: 1;
+bottom: 125%;
+left: 50%;
+margin-left: -60px;
 
     /* Fade in tooltip */
-    opacity: 0;
-    transition: opacity 0.3s;
+opacity: 0;
+transition: opacity 0.3s;
 }
 
 /* Tooltip arrow */
 .tooltip .tooltiptext::after {
-    content: "";
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    margin-left: -5px;
-    border-width: 5px;
-    border-style: solid;
-    border-color: #555 transparent transparent transparent;
+content: "";
+position: absolute;
+top: 100%;
+left: 50%;
+margin-left: -5px;
+border-width: 5px;
+border-style: solid;
+border-color: #555 transparent transparent transparent;
 }
 
 /* Show the tooltip text when you mouse over the tooltip container */
 .tooltip:hover .tooltiptext {
-    visibility: visible;
-    opacity: 1;
+visibility: visible;
+opacity: 1;
 }
     `;
 
@@ -305,11 +305,17 @@ More information that probably helps more then my explanation:<br>
         if (chkCustom.checked == true){
             customSearch = true;
             if (customSearchInput = document.getElementById("chkcustominput").value == ""){
-                customSearchInput = "Ha you can't find this!"
                 customInputTextField = false;
+                if (customSearchInput = "Ha you can't find this!"){
+                    customSearchInput = "Ha you can't find this!";
+                }
             } else {
-                customSearchInput = document.getElementById("chkcustominput").value;
                 customInputTextField = true;
+                if (customSearchInput = document.getElementById("chkcustominput").value == ""){
+                    customSearchInput = "Ha you can't find this!";
+                } else {
+                    customSearchInput = document.getElementById("chkcustominput").value;
+                }
             }
         } else {
             customSearch = false;
