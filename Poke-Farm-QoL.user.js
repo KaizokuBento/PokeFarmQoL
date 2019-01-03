a141c6225ac6cd1d5be57715c543412b37e5aa54
KaizokuBento <bentokaizoku@gmail.com>
2019-01-03 17:18:30 +0100

    Update Poke-Farm-QoL.user.js

--- Poke-Farm-QoL.user.js
+++ Poke-Farm-QoL.user.js
@@ -6,15 +6,18 @@
 // @downloadURL  https://github.com/KaizokuBento/PokeFarmShelter/raw/master/Poke-Farm-QoL.user.js
 // @description  Quality of Life changes to Pokéfarm!
 // @match        https://pokefarm.com/*
+// @resource     QoLSettingsMenuHTML    https://raw.githubusercontent.com/KaizokuBento/PokeFarmQoL/master/resources/templates/qolSettingsMenuHTML.html
+// @resource     shelterSettingsHTML    https://raw.githubusercontent.com/KaizokuBento/PokeFarmQoL/master/resources/templates/shelterOptionsHTML.html
 // @version      0.0.1
 // @grant        GM_getResourceText
-// ==/UserScript==
+// ==/UserScript== 
+
 
 (function() {
     'use strict';
     /////////////////////////////////////
     // Welcome to my first ever script!//
-    // Let's setup the Settings first. //s
+    // Let's setup the Settings first. //
     /////////////////////////////////////
 		
 	let PFQoL = (function PFQoL() {
@@ -42,28 +45,8 @@
 		
 		const TEMPLATES = {
 			headerSettingsLinkHTML	: `<a href=https://pokefarm.com/farm#tab=1>QoL Userscript Settings</a href>`,
-			qolSettingsMenuHTML		: `
-		<div class = panel>
-			<h3>QoL Settings</h3>
-			<div>
-				<p>Settings Page for the Poké Farm QoL Userscript! Check which features you want to enable.</p>
-				<table>
-					<tbody>
-						<tr>
-							<td>
-								<label>
-									<input id="shelterOption" type="checkbox" v-model="userSettings.shelterEnable"> Advanced Shelter Search
-								</label>
-							</td>
-							<td>
-								<button type = "submit" button id = "saveusersetting">Save Settings</button>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>`,
-			shelterSettingsHTML		: GM_getResourceText('QoLHeaderHTML'),
+			qolSettingsMenuHTML		: GM_getResourceText('QoLSettingsMenuHTML')
+			shelterSettingsHTML		: GM_getResourceText('shelterOptionsHTML'),
 	
 		const QOL_CSS = `
 		Empty for now
