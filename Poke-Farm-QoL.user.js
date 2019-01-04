ca7f9b9d6854fe9244e02ef1cdd95bdcabd3d9d7
KaizokuBento <bentokaizoku@gmail.com>
2019-01-04 11:29:11 +0100

    asd

--- Poke-Farm-QoL.user.js
+++ Poke-Farm-QoL.user.js
@@ -7,8 +7,6 @@
 // @description  Quality of Life changes to Pokéfarm!
 // @match        https://pokefarm.com/*
 // @require      http://code.jquery.com/jquery-3.3.1.min.js
-// @require      https://raw.githubusercontent.com/lodash/lodash/4.17.4/dist/lodash.min.js
-// @require      https://cdn.jsdelivr.net/npm/vue
 // @resource     QoLSettingsMenuHTML    https://raw.githubusercontent.com/KaizokuBento/PokeFarmQoL/master/resources/templates/qolSettingsMenuHTML.html
 // @resource     shelterSettingsHTML    https://raw.githubusercontent.com/KaizokuBento/PokeFarmQoL/master/resources/templates/shelterOptionsHTML.html
 // @resource     QoLCSS                 https://raw.githubusercontent.com/KaizokuBento/PokeFarmQoL/master/resources/css/pfqol.css
@@ -23,13 +21,15 @@
     'use strict';
     /////////////////////////////////////
     // Welcome to my first ever script!//
-    // Let's setup the Settings first. //
+    // Let's hope everything works~    //
     /////////////////////////////////////
 
 	let PFQoL = (function PFQoL() {
 
-		const DEFAULT_USER_SETTINGS = {
+		const DEFAULT_USER_SETTINGS = { // default settings when the script gets loaded the first time
+		//userscript settings
 		shelterEnable: true,
+		//shelter settings
 		shelterSettings: {
 			newEgg: true,
 			newPokemon: true,
@@ -47,31 +47,29 @@
 			},
 		};
 
-		const SETTINGS_SAVE_KEY = 'QoLSettings';
+		const SETTINGS_SAVE_KEY = 'QoLSettings'; // the name of the local storage
 		
-		const VARIABLES = {
-			settings : DEFAULT_USER_SETTINGS,
+		const VARIABLES = { // all the variables that are going to be used in fn
+			userSettings : DEFAULT_USER_SETTINGS,
 		}
 
-		const TEMPLATES = {
+		const TEMPLATES = { // all the new/changed HTML for the userscript
 			headerSettingsLinkHTML	: `<a href=https://pokefarm.com/farm#tab=1>QoL Userscript Settings</a href>`,
 			qolSettingsMenuHTML		: GM_getResourceText('QoLSettingsMenuHTML'),
 			shelterSettingsHTML		: GM_getResourceText('shelterSettingsHTML'),
 		};
 
-		const fn = {
+		const fn = { // all the functions for the script
 			/** background stuff */
-			backwork : {
-				setupHTML() {
-					//No clue yet
-					
-					
+			backwork : { // background, stuff the user won't interact with but is needed for the userscript to work
+				setupHTML() { // injects the HTML changes from TEMPLATES into the site
 					// Header link to Userscript settings
 					document.querySelector('#head-right').insertAdjacentHTML('beforebegin', TEMPLATES.headerSettingsLinkHTML);
 
 					// QoL userscript Settings Menu in farmnews
 					if(window.location.href.indexOf("farm#tab=1") != -1){ // Creating the QoL Settings Menu in farmnews
 						document.querySelector('#farmnews').insertAdjacentHTML("afterbegin", TEMPLATES.qolSettingsMenuHTML);
+						
 					}
 					
 					// shelter Settings Menu
@@ -80,22 +78,24 @@
 						document.querySelector("#shelteroptionsqol").insertAdjacentHTML("beforebegin", "<h3>QoL Settings</h3>");
 					}
 				},
-				setupCSS() {
+				setupCSS() { // All the CSS changes are added here
 					GM_addStyle(GM_getResourceText('QoLCSS'));
 				},
-				setupSettings() {
-					if (localStorage.getItem("SETTINGS_SAVE_KEY") === null) {
+				setupInitialSettings() { // Makes the local storage save key QoLSettings with default user settings on first start
+					if (localStorage.getItem(SETTINGS_SAVE_KEY) === null) {
+						localStorage.setItem(SETTINGS_SAVE_KEY, JSON.stringify(VARIABLES.settings));
+					} else {
 						localStorage.setItem(SETTINGS_SAVE_KEY, JSON.stringify(VARIABLES.settings));
 					}
 				},
-				startup() {
+				startup() { // All the functions that are run to start the script on Pokéfarm
 					return {
 						'setting up CSS'	: fn.backwork.setupCSS,
 						'setting up HTML' 	: fn.backwork.setupHTML,
-						'setting settings'	: fn.backwork.setupSettings,
+						'setting settings'	: fn.backwork.setupInitialSettings,
 					}
 				},
-				init() {
+				init() { // Starts all the functions.
 						console.log('Starting up ..');
 						let startup = fn.backwork.startup();
 						for (let message in startup) {
@@ -106,13 +106,21 @@
 							startup[message]();
 						}
 					},
-			},
+			}, // end of backwork
 
 			/** public stuff */
-			API : {
-				
-			},
-		};
+			API : { // the actual seeable and interactable part of the userscript
+				userscriptSettings () {
+					if(window.location.href.indexOf("farm#tab=1") != -1){
+						if($('#shelterEnable').prop('checked')) {
+							alert("It's checked");
+						} else {
+							alert("you unchecked");
+						}
+					}
+				},
+			}, // end of API
+		}; // end of fn
 
 		fn.backwork.init();
 
