a49f46bc01b9c8c833b0d32d72edb592e7e896f2
KaizokuBento <bentokaizoku@gmail.com>
2019-01-05 12:53:08 +0100

    Settings
    Settings are done :)

--- Poke-Farm-QoL.user.js
+++ Poke-Farm-QoL.user.js
@@ -29,29 +29,27 @@
 
 		const DEFAULT_USER_SETTINGS = { // default settings when the script gets loaded the first time
 		//shelter settings
-		shelterSettings: {
-			shelterEnable: true,
-			findNewEgg: false,
-			findNewPokemon: false,
-			findShiny: false,
-			findAlbino: false,
-			findMelanistic: false,
-			findPrehistoric: false,
-			findDelta: false,
-			findMega: false,
-			findStarter: false,
-			findCustomSprite: false,
-			findMale: false,
-			findFemale: false,
-			findNoGender: false,
-			},
+		shelterEnable: true,
+		findNewEgg: true,
+		findNewPokemon: true,
+		findShiny: true,
+		findAlbino: true,
+		findMelanistic: true,
+		findPrehistoric: true,
+		findDelta: true,
+		findMega: true,
+		findStarter: true,
+		findCustomSprite: true,
+		findMale: true,
+		findFemale: true,
+		findNoGender: true,
+		findCustom: null,
 		};
 
 		const SETTINGS_SAVE_KEY = 'QoLSettings'; // the name of the local storage
 
 		const VARIABLES = { // all the variables that are going to be used in fn
 			userSettings : DEFAULT_USER_SETTINGS,
-			loadedSettings : JSON.parse(localStorage.getItem(SETTINGS_SAVE_KEY)),
 		}
 
 		const TEMPLATES = { // all the new/changed HTML for the userscript
@@ -64,14 +62,53 @@
 		}
 
 		const fn = { // all the functions for the script
+			helpers: {
+				toggleSetting(key, set = false) {
+                    if (typeof set === 'boolean') {
+                        let element = document.querySelector(`.qolsetting[data-key="${key}"]`);
+                        if (element && element.type === 'checkbox') {
+                            element.checked = set;
+                        }
+                    }
+                },
+			},
 			/** background stuff */
 			backwork : { // backgrounds tuff
-				loadSettings() { // initial settings if they do not exist (first run)
+				loadSettings() { // initial settings on first run and setting the variable settings key
 					if (localStorage.getItem(SETTINGS_SAVE_KEY) === null) {
 						fn.backwork.saveSettings();
+					} else if (localStorage.getItem(SETTINGS_SAVE_KEY) != VARIABLES.userSettings) {
+						VARIABLES.userSettings = JSON.parse(localStorage.getItem(SETTINGS_SAVE_KEY));
 					}
 				},
+				saveSettings() { // Save changed settings
+					localStorage.setItem(SETTINGS_SAVE_KEY, JSON.stringify(VARIABLES.userSettings));
+				},
+				populateSettingsPage() {
+                    for (let key in VARIABLES.userSettings) {
+                        if (!VARIABLES.userSettings.hasOwnProperty(key)) {
+                            continue;
+                        }
+                        let value = VARIABLES.userSettings[key];
+                        if (typeof value === 'boolean') {
+                            fn.helpers.toggleSetting(key, value, false);
+                            continue;
+                        }
 
+                        if (true === _.isPlainObject(value)) {
+                            for (let key2 in value) {
+                                if (!value.hasOwnProperty(key2)) {
+                                    continue;
+                                }
+                                let value2 = value[key2];
+                                if (typeof value2 === 'boolean') {
+                                    fn.helpers.toggleSetting(`${key}-${key2}`, value2, false);
+                                }
+                            }
+                        }
+                    }
+                },
+				
 				setupHTML() { // injects the HTML changes from TEMPLATES into the site
 
 					// Header link to Userscript settings
@@ -80,20 +117,18 @@
 					// QoL userscript Settings Menu in farmnews
 					if(window.location.href.indexOf("farm#tab=1") != -1){ // Creating the QoL Settings Menu in farmnews
 						document.querySelector('#farmnews').insertAdjacentHTML("afterbegin", TEMPLATES.qolSettingsMenuHTML);
+						fn.backwork.populateSettingsPage();
 					}
 
 					// shelter Settings Menu
-					if (window.location.href.indexOf("shelter") != -1) {
+					if (VARIABLES.userSettings.shelterEnable === true && window.location.href.indexOf("shelter") != -1) {
 						document.querySelector("#shelterupgrades").insertAdjacentHTML("afterend", TEMPLATES.shelterSettingsHTML);
 						document.querySelector("#shelteroptionsqol").insertAdjacentHTML("beforebegin", "<h3>QoL Settings</h3>");
+						fn.backwork.populateSettingsPage();
 					}
 				},
 				setupCSS() { // All the CSS changes are added here
 					GM_addStyle(GM_getResourceText('QoLCSS'));
-				},
-
-				saveSettings() { // Save changed settings
-					localStorage.setItem(SETTINGS_SAVE_KEY, JSON.stringify(VARIABLES.userSettings));
 				},
 
 				startup() { // All the functions that are run to start the script on Pokéfarm
@@ -118,93 +153,20 @@
 
 			/** public stuff */
 			API : { // the actual seeable and interactable part of the userscript
-				userSettingsChange () { // change the usersettings
-					if ($('#shelterOn').prop("checked")) {
-						VARIABLES.userSettings.shelterEnable = true;
-					} else {
-						VARIABLES.userSettings.shelterEnable = false;
-					}
-					fn.backwork.saveSettings();
-				},
-
 				settingsChange (element) {
 					if (JSON.stringify(VARIABLES.userSettings).indexOf(element) >= 0) {
 						console.log("this is "+element);
-						console.log("is this working?"+JSON.stringify(VARIABLES.userSettings.shelterSettings[element]));
-						if (JSON.stringify(VARIABLES.userSettings.shelterSettings[element]) === "false" ) {
-							console.log("was false now true");
-							VARIABLES.userSettings.shelterSettings[element] = true;
-						} else if (JSON.stringify(VARIABLES.userSettings.shelterSettings[element]) === "true" ) {
-							console.log("was true now false");
-							VARIABLES.userSettings.shelterSettings[element] = false;
+						console.log("is this working?"+VARIABLES.userSettings[element]);
+						if (VARIABLES.userSettings[element] === false ) {
+							console.log("was false now true and a boolean");
+							VARIABLES.userSettings[element] = true;
+						} else if (VARIABLES.userSettings[element] === true ) {
+							console.log("was true now false and a boolean");
+							VARIABLES.userSettings[element] = false;
 						}
 					}
 					fn.backwork.saveSettings();
 				},
-
-				/* shelterSettingsChange () { // change the shelter settings
-					if ($('#chkNewEgg').prop("checked")) {
-						VARIABLES.userSettings.findNewEgg = true;
-					} else {
-						VARIABLES.userSettings.findNewEgg = false;
-					}
-
-					if ($('#chkNewPokemon').prop("checked")) {
-						VARIABLES.userSettings.findNewPokemon = true;
-					} else {
-						VARIABLES.userSettings.findNewPokemon = false;
-					}
-
-					if ($('#chkShiny').prop("checked")) {
-						VARIABLES.userSettings.findShiny = true;
-					} else {
-						VARIABLES.userSettings.findShiny = false;
-					}
-
-					if ($('#chkAlbino').prop("checked")) {
-						VARIABLES.userSettings.findAlbino = true;
-					} else {
-						VARIABLES.userSettings.findAlbino = false;
-					}
-
-					if ($('#chkMelanistic').prop("checked")) {
-						VARIABLES.userSettings.findMelanistic = true;
-					} else {
-						VARIABLES.userSettings.findMelanistic = false;
-					}
-
-					if ($('#chkPrehistoric').prop("checked")) {
-						VARIABLES.userSettings.findPrehistoric = true;
-					} else {
-						VARIABLES.userSettings.findPrehistoric = false;
-					}
-
-					if ($('#chkDelta').prop("checked")) {
-						VARIABLES.userSettings.findDelta = true;
-					} else {
-						VARIABLES.userSettings.findDelta = false;
-					}
-
-					if ($('#chkMega').prop("checked")) {
-						VARIABLES.userSettings.findMega = true;
-					} else {
-						VARIABLES.userSettings.findMega = false;
-					}
-
-					if ($('#chkStarter').prop("checked")) {
-						VARIABLES.userSettings.findStarter = true;
-					} else {
-						VARIABLES.userSettings.findStarter = false;
-					}
-
-					if ($('#chkCustomSprite').prop("checked")) {
-						VARIABLES.userSettings.findCustomSprite = true;
-					} else {
-						VARIABLES.userSettings.findCustomSprite = false;
-					}
-
-					fn.backwork.saveSettings();
-				}, */
 			}, // end of API
 		}; // end of fn
 
@@ -212,13 +174,6 @@
 
 		return fn.API;
 	})(); // end of PFQoL function
-
-	//$(document).on('click', '#QoLSettings', (function() { // save userscript settings
-	//	PFQoL.userSettingsChange ();
-	//}));
-
-	//$(document).on('click', '#shelteroptionsqol', (function() { // save shelter settings
-	//	PFQoL.shelterSettingsChange ();
 
 	$(document).on('change', '.qolsetting', (function() {
 		PFQoL.settingsChange(this.getAttribute('data-key'));
