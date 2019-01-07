a7c5059a0df76cabef77443fccb8f77944f14911
KaizokuBento <31934447+KaizokuBento@users.noreply.github.com>
2019-01-07 14:41:18 +0100

    Update Poke-Farm-QoL.user.js

--- Poke-Farm-QoL.user.js
+++ Poke-Farm-QoL.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Poké Farm QoL
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.1
 // @description  Quality of Life chan
 // @author       Bentomon
 // @match        https://pokefarm.com/*
